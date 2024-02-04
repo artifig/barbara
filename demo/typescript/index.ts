736e3e2c50bc83f67af1baea6d013ff7309630e0
Otto Mättas <ottomattas@gmail.com>
2024-02-04 17:36:24 +0200

    Disable extra models

--- demo/typescript/index.ts
+++ demo/typescript/index.ts
@@ -20,12 +20,12 @@ const humanConfig: Partial<H.Config> = { // user configuration for human, used t
   // modelBasePath: '../../models',
   modelBasePath: 'https://vladmandic.github.io/human-models/models/',
   filter: { enabled: true, equalization: false, flip: false },
-  face: { enabled: true, detector: { rotation: false }, mesh: { enabled: true }, attention: { enabled: false }, iris: { enabled: true }, description: { enabled: true }, emotion: { enabled: true }, antispoof: { enabled: true }, liveness: { enabled: true } },
+  face: { enabled: true, detector: { rotation: false }, mesh: { enabled: false }, attention: { enabled: false }, iris: { enabled: false }, description: { enabled: true }, emotion: { enabled: true }, antispoof: { enabled: true }, liveness: { enabled: true } },
   body: { enabled: false },
   hand: { enabled: false },
   object: { enabled: false },
   segmentation: { enabled: false },
-  gesture: { enabled: true },
+  gesture: { enabled: false },
 };
 
 const human = new H.Human(humanConfig); // create instance of human with overrides from user configuration
